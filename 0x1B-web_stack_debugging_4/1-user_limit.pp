5729f19d8fc98ae15a586d4b525aad3bde1d3277
Awoyemi Victor Adewale <scoveyflashmite@gmail.com>
2023-03-10 02:30:27 +0100

    working on web stack debugging #4

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -2,8 +2,4 @@
 
 adduser holberton
 su - holberton
-{ "holberton":
-	ensure => "present",
-	su - holberton,
-	sudo bash -c "echo '* - nofile 10240' >> /etc/security/limits.conf",
-}
+sudo bash -c "echo '* - nofile 10240' >> /etc/security/limits.conf"
